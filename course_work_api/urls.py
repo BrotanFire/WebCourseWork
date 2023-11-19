23c8563819c02039e991272b1ddcfd990c9ed675
Михаил <brotishkafire@mail.ru>
2023-11-19 16:35:10 +0500

    Started

--- course_work_api/urls.py
+++ course_work_api/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import *
 
+urlpatterns = [
+    path('/usertasks/<int:user_id>', ),
+    path('/subtasks/<int:task_id>',),
+    path('/task/<int:task_id>',)
+]
--- course_work_api/views.py
+++ course_work_api/views.py
@@ -1,2 +1,6 @@
+from rest_framework.generics import *
+from .models import *
+class View(ListAPIView):
+    queryset = UsersTasks.objects.all()
 
 
